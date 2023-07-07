8d466741176d7cb91c380ba65f25b1b416679664
Vladislav Diuzhev <vladdiuzhev@gmail.com>
2023-07-07 19:53:04 +0300

    scene basic example added

---
+++ example/test.py
@@ -0,0 +1,53 @@
+import copy
+
+import cv2
+import numpy as np
+from spatialmath import SE3
+
+from itmobotics_sim.utils.robot import EEState, JointState, Motion
+from itmobotics_sim.pybullet_env.pybullet_world import PyBulletWorld, GUI_MODE
+from itmobotics_sim.utils.controllers import JointVelocitiesController
+
+controller_params = {'kp': np.array([12.0, 12.0, 12.0, 2.0, 2.0, 1.0]), 'kd': np.array([1.0, 5.0, 1.0, 0.05, 0.05, 0.05]) * 40}
+
+test_joint_pose = np.array([0.0, -np.pi/2, -np.pi/2, -np.pi/2, np.pi/2, 0.0])
+test_tf = SE3(0.3, 0.0, 1.0) @ SE3.Rx(np.pi)
+
+target_joint_state = JointState.from_position(test_joint_pose)
+
+target_ee_state = EEState.from_tf(test_tf, 'ee_tool')
+target_motion = Motion.from_states(copy.deepcopy(target_joint_state), copy.deepcopy(target_ee_state))
+
+def main():
+    __sim = PyBulletWorld(gui_mode = GUI_MODE.SIMPLE_GUI, time_step = 0.01, time_scale=1)
+    __sim.add_object('table', 'tests/urdf/table.urdf', save=True)
+    __sim.add_object('socket', 'example/urdf/socket.urdf', SE3(0.4,0.2,0.65), save=True)
+    __sim.add_object('plug', 'example/urdf/plug.urdf', SE3(0.4,-0.2,0.65), save=True)
+
+
+    __robot = __sim.add_robot('tests/urdf/ur5e_pybullet.urdf', SE3(0,0,0.625) , 'robot')
+    __robot.joint_controller_params = controller_params
+    __controller_joint_speed = JointVelocitiesController(__robot)
+
+    __sim.reset()
+
+    # __robot.connect_tool('plug', 'example/urdf/plug.urdf',root_link='ee_tool', tf=SE3(0.0, 0.0, 0.1))
+    __robot.connect_camera('base_cam', 'camera_link')
+
+    __robot.reset_joint_state(JointState.from_position(test_joint_pose))
+    # random_target_state = np.random.uniform(-0.1, 0.1, test_joint_pose.shape)
+    # target_motion.joint_state.joint_velocities = random_target_state
+
+    while True: 
+        # ok = __controller_joint_speed.send_control_to_robot(target_motion)
+        # __sim.sim_step()
+        img, _ = __robot.get_image('base_cam')
+        __sim.sim_step()
+        
+        # print(ok)
+        # img_rgb = cv2.cvtColor(np.float32(img), cv2.COLOR_BGR2RGB)
+        # cv2.imshow('out', img_rgb)
+        # cv2.imwrite(f"{__sim.sim_time}.png",np.float32(img))
+    
+if __name__ == "__main__":
+    main()
